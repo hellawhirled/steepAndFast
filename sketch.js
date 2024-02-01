3502960a1f353ac223e5fb21fc0597b371bb8db1
Stephen Gere <78626099+hellawhirled@users.noreply.github.com>
2024-02-01 09:35:40 -0800

    Add Sketch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
-}
+  background(200);
 
-function draw() {
-  background(220);
+  rotate(-PI/20);
+  for (let i = 0; i < 100; i++) {
+    let x = random(width);
+    let y = random(height);
+    let size = random(5);
+
+    noStroke();
+    fill(255);
+    ellipse(x, y, size, size*random(40));
+  }
 }
